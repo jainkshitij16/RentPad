ce86d77d6198fb60598af051da2aa9e5e91aaaa5
Kshitij Jain <jainkshitij16@outlook.com>
2017-03-18 19:05:20 -0700

    Layout and MainActivity

---
+++ app/src/main/java/com/tryout/rentpad/RentAPI_request.java
@@ -0,0 +1,15 @@
+package com.tryout.rentpad;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by Kshitij on 2017-03-18.
+ */
+public class RentAPI_request extends AsyncTask<Void,Void,Void> {
+
+
+    @Override
+    protected Void doInBackground(Void... params) {
+        return null;
+    }
+}
